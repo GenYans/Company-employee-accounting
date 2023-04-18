1ee4e05a3d58c273038c54a93286a1a9c39a2d62
GenYans <genapiter@yandex.ru>
2023-04-18 16:52:35 +0300

    Lesson №129. React

---
+++ src/components/app/app.js
@@ -0,0 +1,30 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+function App() {
+    const data = [
+        {name: 'Dmitriy D.' , salary: 8000, increase: false, id: 1},
+        {name: 'Konstantin K.' , salary: 11000, increase: false, id: 2},
+        {name: 'Gennady Y.' , salary: 14000, increase: true, id: 3},
+    ];
+    return (
+        <div className="app">
+            <AppInfo />
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+            
+            <EmployeesList data={data}/>
+            <EmployeesAddForm/>
+        </div>
+    );
+}
+
+export default App;
