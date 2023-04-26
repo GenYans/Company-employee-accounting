00dd88741ef6ed33914a0eebc1096a0290e7f0f9
GenYans <genapiter@yandex.ru>
2023-04-26 15:22:36 +0300

    Lesson №136

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -43,30 +43,26 @@ class App extends Component {
                 data: newArr
             }
         });
+        
     }
 
-    onToggleIncrease = (id) => { // Будет изменять парраметр Increase на противоположный true/false
-        this.setState(({data}) => {
-            const index = data.findIndex(elem => elem.id === id);
-
-            const old = data[index];
-            const newItem = {...old, increase: !old.increase};
-            const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)]
-
-            return {
-                data: newArr
-            }
-        })
-    }
-
-    onToggleRise = (id) => { // Будет изменять парраметр Rise сотрудника который идет на повышение 
-        console.log(`Rise this ${id}`)
+    onToggleProp = (id, prop) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
     }
 
     render() {
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
         return (
             <div className="app">
-                <AppInfo />
+                <AppInfo employees={employees} increased={increased}/>
     
                 <div className="search-panel">
                     <SearchPanel/>
@@ -76,8 +72,7 @@ class App extends Component {
                 <EmployeesList 
                 data={this.state.data}
                 onDelete={this.deleteItem}
-                onToggleIncrease={this.onToggleIncrease}
-                onToggleRise={this.onToggleRise}/>
+                onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         );
